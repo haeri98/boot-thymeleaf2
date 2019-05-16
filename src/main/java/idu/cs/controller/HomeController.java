854c43989083c7e188f9ca57b0ee340305b2bdc4
user <a>
2019-05-16 17:28:55 +0900

    commit 20190516

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -3,8 +3,11 @@ package idu.cs.controller;
 import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,10 +27,28 @@ public class HomeController {
 		model.addAttribute("khr", "김해리");
 		return "index";
 	}
+	
+	@GetMapping("/")
+	public String loadWelcome(Model model) {
+		return "welcome";
+	}
+	
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
 		model.addAttribute("users", userRepo.findAll());
 		return "userlist";
+	}
+	
+	@PostMapping("/users")
+	public String createUser(@Valid @RequestBody User user, Model model) {
+		userRepo.save(user);
+		model.addAttribute("users", userRepo.findAll());
+		return "redirect:/users";
+	}
+	
+	@GetMapping("/regform")
+	public String loadRegForm(Model model) {		
+		return "regform";
 	}
 	
 	@GetMapping("/users/{id}")
@@ -43,19 +64,36 @@ public class HomeController {
 		return "user";
 	}
 	
-	@GetMapping("/")
-	public String loadWelcome(Model model) {
-		return "welcome";
+	@DeleteMapping("/users/{id}")
+	//@RequestMapping(value="/users/{id}" method="RquestMethod.DELETE")
+	public String deleteuserById(@PathVariable(value = "id") Long userId,  
+	Model model) throws ResourceNotFoundException {
+		User user = userRepo.findById(userId)
+				.orElseThrow(() -> 
+				new ResourceNotFoundException("not found " + userId ));
+		userRepo.delete(user); // 객체 삭제 -> jpa : record 삭제로 적용
+		model.addAttribute("name", user.getName());
+		return "disjoin";
 	}
 	
-	@GetMapping("/regform")
-	public String loadRegForm(Model model) {		
-		return "regform";
-	}	
-	@PostMapping("/users")
-	public String createUser(@Valid @RequestBody User user, Model model) {
-		userRepo.save(user);
-		model.addAttribute("users", userRepo.findAll());
-		return "redirect:/users";
+	@GetMapping("/disjoin")
+	public String loadDelForm(Model model) {		
+		return "disjoin";
 	}
+	
+	@PutMapping("/users/{id}")
+	//@RequestMapping(value="/users/{id}" method="RquestMethod.DELETE")
+	public ResponseEntity<User> UpdateuserById(@PathVariable(value = "id") Long userId,  
+			@Valid @RequestBody User userDetails, Model model) throws ResourceNotFoundException {
+		// userDetails 폼을 통해 전송된 객체, user는 id로 jpa를 통해 가져온 객체
+		User user = userRepo.findById(userId) //userDetails.getId()
+				.orElseThrow(() -> 
+				new ResourceNotFoundException("not found " + userId ));
+		user.setName(userDetails.getName());
+		user.setCompany(userDetails.getCompany());
+		User userUpdate = userRepo.save(user); // 객체 삭제 -> jpa : record 삭제로 적용
+		return ResponseEntity.ok(userUpdate);
+	}
+	
+	
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,6 +4,16 @@ $(document).ready(function () {
         event.preventDefault();
         fire_ajax_submit();
     });
+    
+    $("#delform").submit(function (event) {
+        event.preventDefault();
+        delete_ajax_submit();
+    });
+    
+    $("#updateform").submit(function (event) {
+        event.preventDefault();
+        update_ajax_submit();
+    });
 
 });
 
@@ -29,6 +39,53 @@ function fire_ajax_submit() {
         success: function (data) {
         	console.log("SUCCESS : ", data);
         	window.location.href = "/users";
+        },
+        error: function (e) {
+            console.log("ERROR : ", e);
+        }
+    });
+
+}
+
+function delete_ajax_submit() {
+
+	var url_id = "/users/"+$('#id').val();
+    console.log(url_id);
+    $.ajax({
+        type: "DELETE",
+        url: url_id,
+        dataType: 'text', // json -> text
+        success: function (result) {
+        	console.log("SUCCESS : ", result);
+        	window.location.href = "/disjoin";
+        },
+        error: function (e) {
+            console.log("ERROR : ", e);
+        }
+    });
+
+}
+
+function update_ajax_submit() {
+ 
+	var data = {
+			"name" : $('#name').val(),
+			"company" : $('#company').val()
+	};
+	var user_id = "/users/"+$('#id').val();
+    var json = JSON.stringify(data);
+    console.log(json);
+    $.ajax({
+        type: "PUT",
+        contentType: "application/json",
+        url: user_id,
+        data: json,
+        dataType: 'text', // json -> text
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+        	console.log("SUCCESS : ", data);
+        	//window.location.href = "/users"; // Get 방식으로 접근
         },
         error: function (e) {
             console.log("ERROR : ", e);

f2dc6151b4ce35046e9941ccca04c41645f98cce
IN302 <a>
2019-05-23 16:17:19 +0900

    190523 commit;

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -40,7 +40,7 @@ public class HomeController {
 	}
 	
 	@PostMapping("/users")
-	public String createUser(@Valid @RequestBody User user, Model model) {
+	public String createUser(@Valid User user, Model model) {
 		userRepo.save(user);
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
@@ -58,11 +58,11 @@ public class HomeController {
 		User user = userRepo.findById(userId)
 				.orElseThrow(() -> 
 				new ResourceNotFoundException("not found " + userId ));
-		model.addAttribute("id", "" + userId);
-		model.addAttribute("name", user.getName());
-		model.addAttribute("company", user.getCompany());
+		model.addAttribute("user", user);
+		//model.addAttribute("name", user.getName());
+		//model.addAttribute("company", user.getCompany());
 		return "user";
-	}
+	} 
 	
 	@DeleteMapping("/users/{id}")
 	//@RequestMapping(value="/users/{id}" method="RquestMethod.DELETE")
@@ -76,15 +76,10 @@ public class HomeController {
 		return "disjoin";
 	}
 	
-	@GetMapping("/disjoin")
-	public String loadDelForm(Model model) {		
-		return "disjoin";
-	}
-	
 	@PutMapping("/users/{id}")
 	//@RequestMapping(value="/users/{id}" method="RquestMethod.DELETE")
-	public ResponseEntity<User> UpdateuserById(@PathVariable(value = "id") Long userId,  
-			@Valid @RequestBody User userDetails, Model model) throws ResourceNotFoundException {
+	public String UpdateuserById(@PathVariable(value = "id") Long userId,  
+			@Valid User userDetails, Model model) throws ResourceNotFoundException {
 		// userDetails 폼을 통해 전송된 객체, user는 id로 jpa를 통해 가져온 객체
 		User user = userRepo.findById(userId) //userDetails.getId()
 				.orElseThrow(() -> 
@@ -92,7 +87,10 @@ public class HomeController {
 		user.setName(userDetails.getName());
 		user.setCompany(userDetails.getCompany());
 		User userUpdate = userRepo.save(user); // 객체 삭제 -> jpa : record 삭제로 적용
-		return ResponseEntity.ok(userUpdate);
+		// model.addAttribute("user", userUpdate);
+		return "redirect:/users";
+		// 업데이트가 성공하면 users 자원을 get 방식으로 접근하되 model에 user 어트리뷰트를 전달
+		// return ResponseEntity.ok(userUpdate);
 	}
 	
 	

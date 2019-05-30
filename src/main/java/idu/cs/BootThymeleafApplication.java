5acd8f80719315686e45a793c972cfcd4c8c6a80
haeri98 <rlagfl9580@naver.com>
2019-05-30 14:27:24 +0900

    kanox 파일 임포트

--- src/main/java/idu/cs/BootThymeleafApplication.java
+++ src/main/java/idu/cs/BootThymeleafApplication.java
@@ -3,7 +3,9 @@ package idu.cs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 다양한 어노테이션을 합쳐놓은 기능을 갖고 있다.
+// @EnableAutoConfiguration(execlude = {DataSourceAutoConfiguration.class})
+// h2는 오토로 가능, jpa 오토 불가능
 public class BootThymeleafApplication {
 
 	public static void main(String[] args) {

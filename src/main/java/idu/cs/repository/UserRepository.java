dfc479115600cf43b7d98be0875a5bda128b2044
haeri98 <rlagfl9580@naver.com>
2019-05-30 17:52:27 +0900

    회원정보추가

--- src/main/java/idu/cs/repository/UserRepository.java
+++ src/main/java/idu/cs/repository/UserRepository.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import idu.cs.domain.User;
+import idu.cs.domain.UserEntity;
 
 public interface UserRepository 
-	extends JpaRepository<User, Long> {
+	extends JpaRepository<UserEntity, Long> {
 	// User findById(Id); 만들어져 있음
-	User findByUserId(String userId); // id: 자동 증가번호, userId: 회원가입 아이디 find:select, by:where
-	List<User> findByName(String name);
-	List<User> findByNameOrderByIdAsc(String name); //oderby: oder by
-	List<User> findByCompany(String company);
+	UserEntity findByUserId(String userId); // id: 자동 증가번호, userId: 회원가입 아이디 find:select, by:where
+	List<UserEntity> findByName(String name);
+	List<UserEntity> findByNameOrderByIdAsc(String name); //oderby: oder by
+	List<UserEntity> findByCompany(String company);
 }
